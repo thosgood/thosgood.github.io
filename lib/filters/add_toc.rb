e12d1aa60d4f46752024831abc757d01e621c159
Tim Hosgood <tim.hosgood@gmail.com>
2026-09-23 04:50:20 +0100

    wow flexboxes alone do a LOT

--- lib/filters/add_toc.rb
+++ lib/filters/add_toc.rb
@@ -8,12 +8,15 @@ class AddTocFilter < Nanoc::Filter
     # headers = doc.css('#post-content h1').drop(1)
     headers = doc.css('#post-content h1')
     return doc.to_s if headers.empty?
-    tag_list = doc.css('p')[0]
+    
+    post_content = doc.css('#post-content')[0]
+    post_content.add_previous_sibling(Nokogiri::HTML.fragment("<ol id=\"toc\"></ol>"))
+
     toc_items = headers.map { |header| "<li><a href=\"\##{header["id"]}\">#{header.text}</a></li>" }
-    tag_list.add_previous_sibling(Nokogiri::HTML.fragment("<ol id=\"toc\"></ol>"))
     toc_items.each do |item|
       doc.at_css('#toc').add_child(Nokogiri::HTML.fragment(item))
     end
+
     # # "Scroll to top" links
     # headers.each do |header|
     #   header.add_next_sibling(Nokogiri::HTML.fragment('<a href="#post-title" class="toc-back">&#8607;</a>'))
